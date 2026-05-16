11200997dc457f6565e1b876596093ac7b9f6aad
garyfm <garyfmullen@gmail.com>
2026-05-16 12:30:04 +0100

    Migrate to lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,3 @@
-require('option')
 require('remap')
 require('plugin')
+require('option')
--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -1,119 +1,98 @@
--- Install Packer automatically if it's not installed(Bootstraping)
--- Hint: string concatenation is done by `..`
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
--- Reload configurations if we modify plugins.lua
--- Hint
---     <afile> - replaced with the filename of the buffer being manipulated
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+require("lazy").setup({
+    { "ellisonleao/gruvbox.nvim" },
+    { "maxmx03/fluoromachine.nvim" },
+    --{ dir = "/home/garyfm/workspace/dotfiles/nvim/my-theme/my-theme.nvim" },
 
--- Install plugins here - `use ...`
--- Packer.nvim hints
---     after = string or list,           -- Specifies plugins to load before this plugin. See "sequencing" below
---     config = string or function,      -- Specifies code to run after this plugin is loaded
---     requires = string or list,        -- Specifies plugin dependencies. See "dependencies".
---     ft = string or list,              -- Specifies filetypes which load this plugin.
---     run = string, function, or table, -- Specify operations to be run after successful installs/updates of a plugin
-return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+    },
 
-	---------------------------------------
-	-- NOTE: PUT YOUR THIRD PLUGIN HERE --
-	---------------------------------------
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    { "mbbill/undotree" },
 
-	use({ "ellisonleao/gruvbox.nvim" })
-	use({ "maxmx03/fluoromachine.nvim" })
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    {
+        "mason-org/mason.nvim",
+        build = ":MasonUpdate",
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "clang-format",
+                "cmake-language-server",
+                "cmakelang",
+                "codespell",
+                "cpplint",
+                "cpptools",
+                "lua-language-server",
+                "luacheck",
+                "stylua",
+            },
+        },
+    },
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    -- mason-lspconfig bridges Mason <-> vim.lsp.config
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = { "mason-org/mason.nvim" },
+    },
 
-	use({ "mbbill/undotree" })
+    -- Completion
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip", -- recommended: connect LuaSnip to cmp
+        },
+    },
 
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons", -- optional
-		},
-	})
-	use({
-		"williamboman/mason.nvim",
-		run = function()
-			pcall(vim.cmd, "MasonUpdate")
-		end,
-		opt = {
-			ensure_installed = {
-				"clangd",
-				"clang-format",
-				" cmake-language-server cmake",
-				" cmakelang",
-				" codespell",
-				" cpplint",
-				" cpptools",
-				" lua-language-server lua_ls",
-				" luacheck",
-				" stylua",
-			},
-		},
-	})
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons",
+    },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
-		},
-	})
+    -- Formatter
+    { "mhartington/formatter.nvim" },
 
-	use({ "akinsho/bufferline.nvim", tag = "*", requires = "nvim-tree/nvim-web-devicons" })
+    -- Code outline
+    { 
+        "stevearc/aerial.nvim" 
+    },
 
-	use({ "mhartington/formatter.nvim" })
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
 
-	use({ "stevearc/aerial.nvim" })
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
 
-	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	})
-
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
-
-	use({ "folke/trouble.nvim" })
-
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+    -- Diagnostics
+    { "folke/trouble.nvim" },
+})
