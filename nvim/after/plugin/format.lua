7af50bc04b828c88902cbeb2660a288ba1dcf11b
garyfm <garyfmullen@gmail.com>
2026-05-22 21:42:44 +0100

    Migrate to built in package manager

--- nvim/after/plugin/format.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end,
-})
---
+++ nvim/lua/config/plugin.lua
@@ -0,0 +1,23 @@
+vim.pack.add({
+  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+  { src = "https://github.com/maxmx03/fluoromachine.nvim" },
+
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+
+  { src = "https://github.com/nvim-tree/nvim-tree.lua" },
+
+  { src = "https://github.com/hrsh7th/nvim-cmp" },
+  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+
+  { src = "https://github.com/akinsho/bufferline.nvim" },
+
+  { src = "https://github.com/stevearc/aerial.nvim" },
+
+  { src = "https://github.com/windwp/nvim-autopairs" },
+
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+})
